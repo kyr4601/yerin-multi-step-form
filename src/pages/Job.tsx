36e3b79947c080e909209c3fda7ae45d1f3bc18d
kyr4601 <jane4545@naver.com>
2025-02-18 21:30:29 +0900

    Feat: Job 페이지 버튼 활성화 기능 추가

--- src/pages/Job.tsx
+++ src/pages/Job.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import Button from '../components/Button';
 import s from '../styles.module.css';
 
 const Job = () => {
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  function handleSelect(e: React.ChangeEvent<HTMLSelectElement>) {
+    if (e.target.value !== '선택') {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }
   return (
     <section className={s.container}>
       <h1>직군을 선택해주세요</h1>
-      <select name="jobs">
+      <select name="jobs" onChange={handleSelect}>
+        <option value="선택">선택</option>
         <option value="프론트엔드">프론트엔드</option>
         <option value="백엔드">백엔드</option>
         <option value="iOS">iOS</option>
@@ -14,7 +25,9 @@ const Job = () => {
       </select>
       <div className={s.btnContainer}>
         <Button type={'prev'}>이전</Button>
-        <Button type={'next'}>다음</Button>
+        <Button type={'next'} isDisabled={isDisabled}>
+          다음
+        </Button>
       </div>
     </section>
   );
