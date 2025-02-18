92a6c9c65e0fa7aad27da647f65dbd8c4d32b1b2
kyr4601 <jane4545@naver.com>
2025-02-18 17:08:14 +0900

    Feat: name 페이지 틀 완료

--- src/pages/Name.tsx
+++ src/pages/Name.tsx
@@ -1,9 +1,17 @@
+import Button from '../components/Button';
+import s from '../styles.module.css';
+
 const Name = () => {
   return (
-    <div>
-      name
+    <div className={s.container}>
+      <h1>이름을 입력해주세요</h1>
+      <input type="text" />
+      <div className={s.btnContainer}>
+        <Button type={'prev'}>이전</Button>
+        <Button type={'next'}>다음</Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Name
+export default Name;
