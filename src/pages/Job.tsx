f917ac9e77e205b40defee7ae564e3d3ec8fc4cc
kyr4601 <jane4545@naver.com>
2025-02-18 18:12:52 +0900

    Feat: Job 페이지 틀 완료

--- src/pages/Job.tsx
+++ src/pages/Job.tsx
@@ -1,5 +1,23 @@
+import Button from '../components/Button';
+import s from '../styles.module.css';
+
 const Job = () => {
-  return <div>job</div>;
+  return (
+    <section className={s.container}>
+      <h1>직군을 선택해주세요</h1>
+      <select name="jobs">
+        <option value="프론트엔드">프론트엔드</option>
+        <option value="백엔드">백엔드</option>
+        <option value="iOS">iOS</option>
+        <option value="안드로이드">안드로이드</option>
+        <option value="Devops">Devops</option>
+      </select>
+      <div className={s.btnContainer}>
+        <Button type={'prev'}>이전</Button>
+        <Button type={'next'}>다음</Button>
+      </div>
+    </section>
+  );
 };
 
 export default Job;
