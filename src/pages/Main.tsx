09b84b25615bbd1d23cdeaa3acaabaf4391ecc07
kyr4601 <jane4545@naver.com>
2025-02-18 21:56:27 +0900

    Fix: main 페이지 버튼 컴포넌트로 변경

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,17 +1,11 @@
-import { useNavigate } from 'react-router';
 import s from '../styles.module.css';
+import Button from '../components/Button';
 
 const Main = () => {
-  const navigate = useNavigate();
-
-  function handleNextBtn() {
-    navigate('/name');
-  }
-
   return (
     <section className={s.container}>
       <h1>시작!</h1>
-      <button onClick={handleNextBtn}>작성하기</button>
+      <Button type={'next'}>작성하기</Button>
     </section>
   );
 };
