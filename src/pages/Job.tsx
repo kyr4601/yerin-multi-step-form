bfe24855b2a00b66d71ff13f22a1d95814b807cb
kyr4601 <jane4545@naver.com>
2025-02-19 14:41:25 +0900

    Feat: job 페이지 상태 저장 기능 추가

--- src/pages/Job.tsx
+++ src/pages/Job.tsx
@@ -3,15 +3,20 @@ import Button from '../components/Button';
 import s from '../styles.module.css';
 
 const Job = () => {
-  const [isDisabled, setIsDisabled] = useState(true);
+  const [selected, setSelected] = useState(() => {
+    return localStorage.getItem('job') || '선택';
+  });
+  const [isDisabled, setIsDisabled] = useState(selected === '선택');
 
   function handleSelect(e: React.ChangeEvent<HTMLSelectElement>) {
     setIsDisabled(e.target.value === '선택');
+    setSelected(e.target.value);
+    localStorage.setItem('job', e.target.value);
   }
   return (
     <main className={s.container}>
       <h1>직군을 선택해주세요</h1>
-      <select name="jobs" onChange={handleSelect}>
+      <select defaultValue={selected} name="jobs" onChange={handleSelect}>
         <option value="선택">선택</option>
         <option value="프론트엔드">프론트엔드</option>
         <option value="백엔드">백엔드</option>
