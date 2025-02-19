e43ea5883bab627ac7343923bf80df6a26bbd99b
kyr4601 <jane4545@naver.com>
2025-02-19 14:26:05 +0900

    Feat: name 페이지 상태 저장 기능 추가

--- src/pages/Name.tsx
+++ src/pages/Name.tsx
@@ -1,17 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from '../components/Button';
 import s from '../styles.module.css';
 
 const Name = () => {
-  const [isDisabled, setIsDisabled] = useState(true);
+  const [name, setName] = useState(() => {
+    return localStorage.getItem('name') || '';
+  });
+  const [isDisabled, setIsDisabled] = useState(name === '');
+
+  useEffect(() => {
+    const debouncedInput = setTimeout(() => {
+      localStorage.setItem('name', name);
+    }, 500);
+
+    return () => clearTimeout(debouncedInput);
+  }, [name]);
 
   function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
     setIsDisabled(e.target.value.length < 1);
   }
+
   return (
     <main className={s.container}>
       <h1>이름을 입력해주세요</h1>
-      <input type="text" onChange={handleInput} />
+      <input type="text" value={name} onChange={handleInput} />
       <div className={s.btnContainer}>
         <Button type={'prev'}>이전</Button>
         <Button type={'next'} isDisabled={isDisabled}>
